91042b96d57e7318b08b00505027fd14a6853031
Aman Ranjan <aman.ranjan@mapmyindia.com>
2026-09-11 12:09:51 +0530

    Bump version to 1.0.2 and add project configuration files

--- src/pysmrf/__init__.py
+++ src/pysmrf/__init__.py
@@ -19,7 +19,7 @@ And the software implementation:
 
 from __future__ import annotations
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Aman Ranjan; Thomas Pingel (original algorithm)"
 __email__ = "er.amanranjan@gmail.com"
 __license__ = "MIT"
